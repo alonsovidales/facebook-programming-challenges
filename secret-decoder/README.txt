035c12a7ed91001d19e0b2188cc690402af101d2
Alonso Vidales <alonso.vidales@tras2.es>
2012-12-09 23:26:33 +0100

    Added Secret decoder test, Facebook made a mistake on the results, and canot be submited :'(

---
+++ secret-decoder/README.txt
@@ -0,0 +1,27 @@
+Your task is to decode messages that were encoded with substitution ciphers. In a substitution cipher, all occurrences of a character are replaced by a different character. For example, in a cipher that replaces "a" with "d" and "b" with "e", the message "abb" is encoded as "dee".
+
+The exact character mappings that are used in the substitution ciphers will not be known to you. However, the dictionary of words that were used will be given. You will be given multiple encoded messages to decode (one per line) and they may use different substitution ciphers. The same substitution cipher is used on all of the words in a particular message.
+
+For each scrambled message in the input, your program should output a line with the input line, followed by the string " = " (without the quotes), followed by the decoded message.
+
+NOTE: All inputs are from stdin and output to stdout. The input will be exactly like how it's given in the problem and
+
+your output should exactly match the given example output
+
+Example:
+
+input file:
+
+//dict
+hello
+there
+yello
+thorns
+//secret
+12334 51272
+12334 514678
+
+
+output:
+12334 51272 = hello there
+12334 514678 = hello thorns
---
+++ secret-decoder/secret_decoder.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+"""
+The test case is not corret!!!!!!
+Usign the given input:
+//dict
+hello
+there
+yello
+bob
+tom
+mabel
+says
+hi
+secret
+the
+is
+to
+smile
+//secret
+45161 01223
+x2x 3453 6k
+x8z yz67zx 5y x4 y352z
+
+According to the verification system, the output should to be:
+there yello
+bob says hi
+tom secret is to smile
+
+as you can see on the third line we have:
+x8z -> tom
+and
+x4 -> to
+The "o" of Tom is an 8 and the "o" of to is an 4
+
+Please, tell me if is my or your mistake, I was mad for two hours trying to solve this problem :'(
+"""
+
+import fileinput, itertools, collections
+
+__author__ = "Alonso Vidales"
+__email__ = "alonso.vidales@tras2.es"
+__date__ = "2012-12-09"
+
+class SecretDecoder:
+    __wordsByLen = {}
+    __originLinesEnc = []
+    __linesEncoded = []
+
+    def __checkIfPossible(self, inLines):
+        print inLines
+        for linePos in xrange(0, len(inLines)):
+            charDict = {}
+            for wordPos in xrange(0, len(inLines[linePos])):
+                for char in xrange(0, len(self.__originLinesEnc[linePos][wordPos])):
+                    if charDict.get(inLines[linePos][wordPos][char], '') == '':
+                        charDict[inLines[linePos][wordPos][char]] = self.__originLinesEnc[linePos][wordPos][char]
+                    else:
+                        print inLines[linePos][wordPos][char] + ' - ' + charDict[inLines[linePos][wordPos][char]] + ' - ' + self.__originLinesEnc[linePos][wordPos][char]
+                        if charDict[inLines[linePos][wordPos][char]] != self.__originLinesEnc[linePos][wordPos][char]:
+                            print "NO"
+                            return False
+
+        return True
+
+    def __checkByDepp(self, inLines, inCurrentLine = 0, inCurrentWord = 0):
+        if self.__checkIfPossible(inLines):
+            if len(inLines) == len(self.__originLinesEnc) and len(inLines[len(inLines) - 1]) == len(self.__originLinesEnc[len(inLines) - 1]):
+                for line in inLines:
+                    print " ".join(line)
+                exit()
+
+            for possibleWord in self.__wordsByLen[len(self.__originLinesEnc[inCurrentLine][inCurrentWord])]:
+                try:
+                    inLines[inCurrentLine][inCurrentWord] = possibleWord
+                except:
+                    try:
+                        inLines[inCurrentLine].insert(inCurrentWord, possibleWord)
+                    except:
+                        inLines.append([possibleWord])
+
+                if len(self.__originLinesEnc[inCurrentLine]) == (inCurrentWord + 1):
+                    self.__checkByDepp(inLines, inCurrentLine + 1, 0)
+                else:
+                    self.__checkByDepp(inLines, inCurrentLine, inCurrentWord + 1)
+
+    def resolve(self):
+        self.__checkByDepp([])
+
+        return ""
+
+    def __init__(self, inInputLines):
+        readingSecrets = False
+        for line in inInputLines[1:]:
+            if line == '//secret':
+                readingSecrets = True
+            else:
+                if readingSecrets:
+                    self.__linesEncoded.append(line.split())
+                    self.__originLinesEnc.append(line.split())
+                else:
+                    if self.__wordsByLen.get(len(line), False) == False:
+                        self.__wordsByLen[len(line)] = [line]
+                    else:
+                        self.__wordsByLen[len(line)].append(line)
+
+        print self.__wordsByLen
+        print self.__linesEncoded
+
+if __name__ == "__main__":
+    lines = [line.replace('\n', '') for line in fileinput.input()]
+
+    print SecretDecoder(lines).resolve()
