a569ac3daff0c3a9590c8f8b430102921babcc17
Alonso Vidales <alonso.vidales@tras2.es>
2013-01-14 00:31:29 +0100

    Verified result

--- secret-decoder/secret_decoder.py
+++ secret-decoder/secret_decoder.py
@@ -1,105 +1,108 @@
 #!/usr/bin/env python
 
-"""
-The test case is not corret!!!!!!
-Usign the given input:
-//dict
-hello
-there
-yello
-bob
-tom
-mabel
-says
-hi
-secret
-the
-is
-to
-smile
-//secret
-45161 01223
-x2x 3453 6k
-x8z yz67zx 5y x4 y352z
-
-According to the verification system, the output should to be:
-there yello
-bob says hi
-tom secret is to smile
-
-as you can see on the third line we have:
-x8z -> tom
-and
-x4 -> to
-The "o" of Tom is an 8 and the "o" of to is an 4
-
-I wasted two hours trying to solve this problem :'(
-"""
-
-import fileinput, itertools, collections
+import fileinput
 
 __author__ = "Alonso Vidales"
 __email__ = "alonso.vidales@tras2.es"
-__date__ = "2012-12-09"
+__date__ = "2013-01-13"
 
 class SecretDecoder:
-    __wordsByLen = {}
-    __originLinesEnc = []
-    __linesEncoded = []
+    __debug = False
+    # Will contain the original lines encoded as lists of words
+    __linesEncoded = None
+    # This dictionary will be used to store all the possible words sorted by length in order to
+    # do as fast as possible the replacement
+    __wordsByLen = None
 
-    def __checkIfPossible(self, inLines):
-        for linePos in xrange(0, len(inLines)):
-            charDict = {}
-            for wordPos in xrange(0, len(inLines[linePos])):
-                for char in xrange(0, len(self.__originLinesEnc[linePos][wordPos])):
-                    if charDict.get(inLines[linePos][wordPos][char], '') == '':
-                        charDict[inLines[linePos][wordPos][char]] = self.__originLinesEnc[linePos][wordPos][char]
-                    else:
-                        if charDict[inLines[linePos][wordPos][char]] != self.__originLinesEnc[linePos][wordPos][char]:
-                            return False
+    def __checkIfPossibleLine(self, inOriginal, inLine):
+        """
+        This method returns true if the inLine is a possible combination compared with
+        the inOringinal line words by words and char by char or false if is not a possible combination
+
+        @return bool True if is possible, False if not
+        """
+        if self.__debug:
+            print inLine
+
+        charDict = {}
+        # Check char by char all the words
+        for wordPos in xrange(0, len(inLine)):
+            for charPos in xrange(0, len(inLine[wordPos])):
+                # Use a dictionary in order to know if the character was previously setted, and
+                # if it was, check if the replacement matches
+                if charDict.get(inLine[wordPos][charPos], '') == '':
+                    charDict[inLine[wordPos][charPos]] = inOriginal[wordPos][charPos]
+                else:
+                    if charDict[inLine[wordPos][charPos]] != inOriginal[wordPos][charPos]:
+                        if self.__debug:
+                            print "No possible"
+
+                        return False
+
+        if self.__debug:
+            print "Possible"
 
         return True
 
-    def __checkByDepp(self, inLines, inCurrentLine = 0, inCurrentWord = 0):
-        if self.__checkIfPossible(inLines):
-            if len(inLines) == len(self.__originLinesEnc) and len(inLines[len(inLines) - 1]) == len(self.__originLinesEnc[len(inLines) - 1]):
-                for line in inLines:
-                    print " ".join(line)
-                exit()
+    def __checkByDepp(self, inOriginalLine, inLine = [], inCurrentWord = 0):
+        """
+        Recursive search, check all the posible combinations for the given line as a string with
+        the necessary format to be printed
+        """
+        if self.__debug:
+            return "Checking: %s - %s" % (inOriginalLine, inLine)
 
-            for possibleWord in self.__wordsByLen[len(self.__originLinesEnc[inCurrentLine][inCurrentWord])]:
-                try:
-                    inLines[inCurrentLine][inCurrentWord] = possibleWord
-                except:
-                    try:
-                        inLines[inCurrentLine].insert(inCurrentWord, possibleWord)
-                    except:
-                        inLines.append([possibleWord])
+        if self.__checkIfPossibleLine(inOriginalLine, inLine):
+            # If the line is possible, and the number of words are the same, we have a
+            # line, return it, this will stop the recursivity :)
+            if len(inOriginalLine) == len(inLine):
+                return "%s = %s" % (' '.join(inOriginalLine), ' '.join(inLine))
 
-                if len(self.__originLinesEnc[inCurrentLine]) == (inCurrentWord + 1):
-                    self.__checkByDepp(inLines, inCurrentLine + 1, 0)
-                else:
-                    self.__checkByDepp(inLines, inCurrentLine, inCurrentWord + 1)
+            # Check all the words with the same lenght that the number of encrypted characters have
+            for word in self.__wordsByLen[len(inOriginalLine[inCurrentWord])]:
+                newLine = inLine[:]
+                newLine.append(word)
+                solution = self.__checkByDepp(inOriginalLine, newLine, inCurrentWord + 1)
+                if solution <> False:
+                    return solution
+
+        return False
 
     def resolve(self):
-        self.__checkByDepp([])
+        """
+        This method launches the recursive serach of the correct combination and returns as string the text
+        decoded if possible, with the necessary format given by the specifications
 
-        return ""
+        @return str The text decoded and formatted
+        """
+        decrypted = []
+        for line in self.__linesEncoded:
+            decrypted.append(self.__checkByDepp(line))
+
+        return '\n'.join(decrypted)
 
     def __init__(self, inInputLines):
+        self.__linesEncoded = []
+        self.__wordsByLen = {}
         readingSecrets = False
+
+        # Parse the input lines and prepare the dictionary of words by length, the lists with the encoded
+        # lines, and the list with the possible words to be used
         for line in inInputLines[1:]:
             if line == '//secret':
                 readingSecrets = True
             else:
                 if readingSecrets:
                     self.__linesEncoded.append(line.split())
-                    self.__originLinesEnc.append(line.split())
                 else:
                     if self.__wordsByLen.get(len(line), False) == False:
                         self.__wordsByLen[len(line)] = [line]
                     else:
                         self.__wordsByLen[len(line)].append(line)
+
+        if self.__debug:
+            print "Encoded: %s" % (self.__linesEncoded)
+            print "Words by len: %s" % (self.__wordsByLen)
 
 if __name__ == "__main__":
     lines = [line.replace('\n', '') for line in fileinput.input()]
